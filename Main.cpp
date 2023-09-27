44c320b8c4c2a0a3ba90ba5fe2d886889ec51456
BrandonBaier-png <144815527+BrandonBaier-png@users.noreply.github.com>
2023-09-27 00:27:19 -0500

    Add files via upload

---
+++ Main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <ctime>; // might not need
+
+using namespace std;
+
+int main() {
+	cout << "Welcome to collection simulator (name pending)";
+
+	int pointTotal = 0;
+
+		/*do {
+
+		} while (totalPoints < 3);
+		*/
+}
